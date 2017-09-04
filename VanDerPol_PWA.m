47873a06755a17e05a2395ed18d3182fa8ac4602
Shen Shen <shenshen@mit.edu>
2017-09-04 13:40:03 -0400

    VanDerPol 1st stage pass and cleanup

--- VanDerPol_PWA.m
+++ VanDerPol_PWA.m
@@ -11,7 +11,6 @@ xdot = [x(2); -x(1)-x(2).*(x(1).^2-1)];
 % df = [0 1; -1-2*x(1)*x(2), -(x(1)^2-1)];
 A_zero=[0 1;-1 1];
 
-% epsi=1e-4*ones(4,1);
 w=sos_version(x,xdot,A_zero);
 % w=lp_version(x,xdot,A_zero);
 plots(w);
@@ -21,80 +20,37 @@ end
 function w=sos_version(x,xdot,A_zero)
 	prog = spotsosprog;
 	prog = prog.withIndeterminate(x);
-	% Vertices conditions
+% Vertices conditions
 	[prog,Vertices_values] = prog.newPos(4);
-
+	% epsi=1e-4*ones(4,1);
 	% prog=prog.withPos(Vertices_values-epsi);
-	% the normals
+	rho=1;
+
+% the normals
 	w1=[-Vertices_values(2);Vertices_values(1)];
 	w2=[-Vertices_values(2);-Vertices_values(3)];
 	w3=[Vertices_values(4);-Vertices_values(3)];
 	w4=[Vertices_values(4);Vertices_values(1)];
-
+ % Lagrange multipliers
 	Lmonom = monomials(x,0:2);
-
 	[prog,L1] = prog.newFreePoly(Lmonom);
 	[prog,L2] = prog.newFreePoly(Lmonom);
 	[prog,L3] = prog.newFreePoly(Lmonom);
-
 	prog = prog.withSOS(L1);
 	prog = prog.withSOS(L2);
 	prog = prog.withSOS(L3);
-
-	% polytope 1
-	V1dot=w1'*xdot;
-	constraint1=x(1);
-	constraint2=-x(2);
-	constraint3=-x(1)+x(2)-1e-0;
-	prog=prog.withSOS(-V1dot+L1*constraint1+L2*constraint2+L3*constraint3);
-
-	[prog,L1] = prog.newFreePoly(Lmonom);
-	[prog,L2] = prog.newFreePoly(Lmonom);
-	[prog,L3] = prog.newFreePoly(Lmonom);
-
-	prog = prog.withSOS(L1);
-	prog = prog.withSOS(L2);
-	prog = prog.withSOS(L3);
-
-	V1dot=w1'*xdot;
-	constraint1=x(1);
-	constraint2=-x(2);
-	constraint3=-x(1)+x(2);
-	prog=prog.withSOS(-V1dot+L1*constraint1+L2*constraint2+L3*constraint3);
-
-
-	% polytope 2
 	[prog,L4] = prog.newFreePoly(Lmonom);
 	[prog,L5] = prog.newFreePoly(Lmonom);
 	[prog,L6] = prog.newFreePoly(Lmonom);
-
 	prog = prog.withSOS(L4);
 	prog = prog.withSOS(L5);
 	prog = prog.withSOS(L6);
-
-	V2dot=w2'*xdot;
-	constraint4=x(1);
-	constraint5=x(2);
-	constraint6=-x(1)-x(2)-1e-0;
-	prog=prog.withSOS(-V2dot+L4*constraint4+L5*constraint5+L6*constraint6);
-
-
-	% polytope 3
 	[prog,L7] = prog.newFreePoly(Lmonom);
 	[prog,L8] = prog.newFreePoly(Lmonom);
 	[prog,L9] = prog.newFreePoly(Lmonom);
-
 	prog = prog.withSOS(L7);
 	prog = prog.withSOS(L8);
 	prog = prog.withSOS(L9);
-
-	V3dot=w3'*xdot;
-	constraint7=x(2);
-	constraint8=-x(1);
-	constraint9=x(1)-x(2)-1e-0;
-	prog=prog.withSOS(-V3dot+L7*constraint7+L8*constraint8+L9*constraint9);
-
-	% polytope 4
 	[prog,L10] = prog.newFreePoly(Lmonom);
 	[prog,L11] = prog.newFreePoly(Lmonom);
 	[prog,L12] = prog.newFreePoly(Lmonom);
@@ -102,16 +58,37 @@ function w=sos_version(x,xdot,A_zero)
 	prog = prog.withSOS(L11);
 	prog = prog.withSOS(L12);
 
+	% polytope 1
+	V1dot=w1'*xdot;
+	constraint1=x(1);
+	constraint2=-x(2);
+	constraint3=-x(1)+x(2)-rho;
+	prog=prog.withSOS(-V1dot+L1*constraint1+L2*constraint2+L3*constraint3);
+
+	% polytope 2
+	V2dot=w2'*xdot;
+	constraint4=x(1);
+	constraint5=x(2);
+	constraint6=-x(1)-x(2)-rho;
+	prog=prog.withSOS(-V2dot+L4*constraint4+L5*constraint5+L6*constraint6);
+
+	% polytope 3
+	V3dot=w3'*xdot;
+	constraint7=x(2);
+	constraint8=-x(1);
+	constraint9=x(1)-x(2)-rho;
+	prog=prog.withSOS(-V3dot+L7*constraint7+L8*constraint8+L9*constraint9);
+
+	% polytope 4
 	V4dot=w4'*xdot;
 	constraint10=-x(1);
 	constraint11=-x(2);
-	constraint12=x(1)+x(2)-1e-0;
+	constraint12=x(1)+x(2)-rho;
 	prog=prog.withSOS(-V4dot+L10*constraint10+L11*constraint11+L12*constraint12);
-
 
 	options = spot_sdp_default_options();
 	options.verbose=1;
-	sol=prog.minimize(-sum(Vertices_values(2)),@spot_mosek,options);
+	sol=prog.minimize(-sum(Vertices_values),@spot_mosek,options);
 	Vertices_values=double(sol.eval(Vertices_values));
 	w1=double((sol.eval(w1)));
 	w2=double(sol.eval(w2));
