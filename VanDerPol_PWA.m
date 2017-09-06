e88484c83fa77620fdc067f5ff7e8da24918b14e
Shen Shen <shenshen@mit.edu>
2017-09-06 00:15:36 -0400

    1st square iteration

--- VanDerPol_PWA.m
+++ VanDerPol_PWA.m
@@ -13,8 +13,10 @@ xdot = [x(2); -x(1)-x(2).*(x(1).^2-1)];
 A_zero=[0 1;-1 1];
 
 [Vertices_values,w,old_rho]=diamond_iteration(x,xdot,A_zero);
-plots(w);
-rho=optimizeRho(x,xdot,Vertices_values,old_rho);
+square_iteration(x,xdot,Vertices_values);
+
+% plots(w);
+% rho=optimizeRho(x,xdot,Vertices_values,old_rho);
 % w=level_set_version(x,xdot,A_zero);
 % w=lp_version(x,xdot,A_zero);
 end
@@ -177,7 +179,7 @@ prog=prog.withSOS(-L12*V4dot+L10*constraint10+L11*constraint11+constraint12);
 options = spot_sdp_default_options();
 options.verbose=0;
 sol=prog.minimize(-sum(rho),@spot_mosek,options);
-rho=double(sol.eval(rho))
+rho=double(sol.eval(rho));
 % w1=double((sol.eval(w1)));
 % w2=double(sol.eval(w2));
 % w3=double(sol.eval(w3));
@@ -187,23 +189,32 @@ rho=double(sol.eval(rho))
 end
 
 
-function square_iteration(x,xdot,delta,diamond_vert_values)
+function square_iteration(x,xdot,diamond_vert_values)
 prog = spotsosprog;
 prog = prog.withIndeterminate(x);
 % Vertices conditions
 [prog,Vertices_values] = prog.newPos(4);
 % epsi=1e-4*ones(4,1);
-% prog=prog.withPos(Vertices_values-epsi);
-rho=1;
+prog=prog.withPos(Vertices_values(1)-diamond_vert_values(1));
+prog=prog.withPos(Vertices_values(1)-diamond_vert_values(2));
+prog=prog.withPos(Vertices_values(2)-diamond_vert_values(2));
+prog=prog.withPos(Vertices_values(2)-diamond_vert_values(3));
+prog=prog.withPos(Vertices_values(3)-diamond_vert_values(3));
+prog=prog.withPos(Vertices_values(3)-diamond_vert_values(4));
+prog=prog.withPos(Vertices_values(4)-diamond_vert_values(4));
+prog=prog.withPos(Vertices_values(4)-diamond_vert_values(1));
+
+rho=1e-4;
 
 % the normals
-w1=[-Vertices_values(2);Vertices_values(1)]./delta;
-w2=[-Vertices_values(2);-Vertices_values(3)]./delta;
-w3=[Vertices_values(4);-Vertices_values(3)]./delta;
-w4=[Vertices_values(4);Vertices_values(1)]./delta;
+w1=[diamond_vert_values(1)-Vertices_values(1);Vertices_values(1)-diamond_vert_values(2)];
+w2=[diamond_vert_values(3)-Vertices_values(2);diamond_vert_values(2)-Vertices_values(2)];
+w3=[Vertices_values(3)-diamond_vert_values(3);diamond_vert_values(4)-Vertices_values(3)];
+w4=[Vertices_values(4)-diamond_vert_values(1);Vertices_values(4)-diamond_vert_values(4)];
+
 % Lagrange multipliers
 Lmonom = monomials(x,0:2);
-[prog,L1] = prog.newFreePoly(Lmonom);
+ [prog,L1] = prog.newFreePoly(Lmonom);
 [prog,L2] = prog.newFreePoly(Lmonom);
 [prog,L3] = prog.newFreePoly(Lmonom);
 prog = prog.withSOS(L1);
@@ -230,36 +241,36 @@ prog = prog.withSOS(L12);
 
 % polytope 1
 V1dot=w1'*xdot;
-constraint1=x(1);
-constraint2=-x(2);
-constraint3=-x(1)+x(2)-rho;
+constraint1=-rho-x(1);
+constraint2=x(2)-rho;
+constraint3=-(-x(1)+x(2)-rho);
 prog=prog.withSOS(-V1dot+L1*constraint1+L2*constraint2+L3*constraint3);
 
 % polytope 2
 V2dot=w2'*xdot;
-constraint4=x(1);
-constraint5=x(2);
-constraint6=-x(1)-x(2)-rho;
+constraint4=-rho-x(1);
+constraint5=-rho-x(2);
+constraint6=-(-x(1)-x(2)-rho);
 prog=prog.withSOS(-V2dot+L4*constraint4+L5*constraint5+L6*constraint6);
 
 % polytope 3
 V3dot=w3'*xdot;
-constraint7=x(2);
-constraint8=-x(1);
-constraint9=x(1)-x(2)-rho;
+constraint7=-rho-x(2);
+constraint8=x(1)-rho;
+constraint9=-(x(1)-x(2)-rho);
 prog=prog.withSOS(-V3dot+L7*constraint7+L8*constraint8+L9*constraint9);
 
 % polytope 4
 V4dot=w4'*xdot;
-constraint10=-x(1);
-constraint11=-x(2);
-constraint12=x(1)+x(2)-rho;
+constraint10=-rho+x(1);
+constraint11=x(2)-rho;
+constraint12=-(x(1)+x(2)-rho);
 prog=prog.withSOS(-V4dot+L10*constraint10+L11*constraint11+L12*constraint12);
 
 options = spot_sdp_default_options();
 options.verbose=0;
 sol=prog.minimize(-sum(Vertices_values),@spot_mosek,options);
-Vertices_values=double(sol.eval(Vertices_values));
+Vertices_values=double(sol.eval(Vertices_values))
 w1=double((sol.eval(w1)));
 w2=double(sol.eval(w2));
 w3=double(sol.eval(w3));
