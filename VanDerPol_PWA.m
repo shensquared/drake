8849099ef3349dfe4959fa1eeff7b8ccf6245f03
Shen Shen <shenshen@mit.edu>
2017-09-08 10:50:10 -0400

    2nd stage linear prog setup

--- VanDerPol_PWA.m
+++ VanDerPol_PWA.m
@@ -13,15 +13,202 @@ function VanDerPol_PWA()
 	sol_OK=true;
 	max_iter=100;
 	epsi=1e-6;
-	while(sol_OK)
-	% 	 disp(iter_count);
-		[rho,Vertices_values,w,sol_OK]=diamond(x,xdot,df,'fix_rho',rho,Vertices_values,w);
-		rho=1.2*rho;
-		iter_count=iter_count+1;
-	end
+
+	last_vertice_values =1.0e-05 *[0.1233;
+    0.1577;
+    0.1228;
+    0.1591;]
+    rho=.0060;
+
+
+	% while(sol_OK)
+	% % 	 disp(iter_count);
+	% 	[rho,Vertices_values,w,sol_OK]=diamond(x,xdot,df,'fix_rho',rho,Vertices_values,w)
+	% 	rho=1.2*rho;
+	% 	iter_count=iter_count+1;
+ %    end
+    diamond_ring(x,xdot,df,rho,last_vertice_values,rho);
 	rho
 	% plots(w);
 end
+
+function diamond_ring(x,xdot,df,last_rho,last_vertice_values,delta_rho)
+	prog = spotsosprog;
+	prog = prog.withIndeterminate(x);
+	% xdot=rho*subs(xdot,x,1/rho*x);
+	[prog,t] = prog.newPos(1);
+	prog=prog.withPos(t*ones(4,1)-last_vertice_values);
+	[prog,this_vert_values] = prog.newPos(4);
+	prog=prog.withPos(this_vert_values-t*ones(4,1));
+
+	Vmonom = monomials(x,0:1);
+	[prog,V1] = prog.newFreePoly(Vmonom);
+	[prog,V2] = prog.newFreePoly(Vmonom);
+	[prog,V3] = prog.newFreePoly(Vmonom);
+	[prog,V4] = prog.newFreePoly(Vmonom);
+
+	vert1=[0;last_rho];
+	vert2=[-last_rho;0];
+	vert3=[0;-last_rho];
+	vert4=[last_rho;0];
+	vert5=[0;last_rho+delta_rho];
+	vert6=[-last_rho-delta_rho;0];
+	vert7=[0;-last_rho-delta_rho];
+	vert8=[last_rho+delta_rho;0];
+
+	prog=prog.withEqs(subs(V1,x,vert1)-last_vertice_values(1));
+	prog=prog.withEqs(subs(V1,x,vert2)-last_vertice_values(2));
+	prog=prog.withEqs(subs(V2,x,vert2)-last_vertice_values(2));
+	prog=prog.withEqs(subs(V2,x,vert3)-last_vertice_values(3));
+	prog=prog.withEqs(subs(V3,x,vert3)-last_vertice_values(3));
+	prog=prog.withEqs(subs(V3,x,vert4)-last_vertice_values(4));
+	prog=prog.withEqs(subs(V4,x,vert4)-last_vertice_values(4));
+	prog=prog.withEqs(subs(V4,x,vert1)-last_vertice_values(1));
+
+	prog=prog.withEqs(subs(V1,x,vert5)-this_vert_values(1));
+	prog=prog.withEqs(subs(V1,x,vert6)-this_vert_values(2));
+	prog=prog.withEqs(subs(V2,x,vert6)-this_vert_values(2));
+	prog=prog.withEqs(subs(V2,x,vert7)-this_vert_values(3));
+	prog=prog.withEqs(subs(V3,x,vert7)-this_vert_values(3));
+	prog=prog.withEqs(subs(V3,x,vert8)-this_vert_values(4));
+	prog=prog.withEqs(subs(V4,x,vert8)-this_vert_values(4));
+	prog=prog.withEqs(subs(V4,x,vert5)-this_vert_values(1));
+
+	
+	% Lmonom = monomials(x,0:2);
+	% [prog,L1] = prog.newFreePoly(Lmonom);
+	% [prog,L2] = prog.newFreePoly(Lmonom);
+	% [prog,L3] = prog.newFreePoly(Lmonom);
+	% prog = prog.withSOS(L1);
+	% prog = prog.withSOS(L2);
+	% prog = prog.withSOS(L3);
+	% [prog,L4] = prog.newFreePoly(Lmonom);
+	% [prog,L5] = prog.newFreePoly(Lmonom);
+	% [prog,L6] = prog.newFreePoly(Lmonom);
+	% prog = prog.withSOS(L4);
+	% prog = prog.withSOS(L5);
+	% prog = prog.withSOS(L6);
+	% [prog,L7] = prog.newFreePoly(Lmonom);
+	% [prog,L8] = prog.newFreePoly(Lmonom);
+	% [prog,L9] = prog.newFreePoly(Lmonom);
+	% prog = prog.withSOS(L7);
+	% prog = prog.withSOS(L8);
+	% prog = prog.withSOS(L9);
+	% [prog,L10] = prog.newFreePoly(Lmonom);
+	% [prog,L11] = prog.newFreePoly(Lmonom);
+	% [prog,L12] = prog.newFreePoly(Lmonom);
+	% prog = prog.withSOS(L10);
+	% prog = prog.withSOS(L11);
+	% prog = prog.withSOS(L12);
+
+	% [prog,L13] = prog.newFreePoly(Lmonom);
+	% [prog,L14] = prog.newFreePoly(Lmonom);
+	% [prog,L15] = prog.newFreePoly(Lmonom);
+	% prog = prog.withSOS(L13);
+	% prog = prog.withSOS(L14);
+	% prog = prog.withSOS(L15);
+	% [prog,L16] = prog.newFreePoly(Lmonom);
+	% [prog,L17] = prog.newFreePoly(Lmonom);
+	% [prog,L18] = prog.newFreePoly(Lmonom);
+	% prog = prog.withSOS(L16);
+	% prog = prog.withSOS(L17);
+	% prog = prog.withSOS(L18);
+	% [prog,L19] = prog.newFreePoly(Lmonom);
+	% [prog,L20] = prog.newFreePoly(Lmonom);
+	% [prog,L21] = prog.newFreePoly(Lmonom);
+	% prog = prog.withSOS(L19);
+	% prog = prog.withSOS(L20);
+	% prog = prog.withSOS(L21);
+	% [prog,L22] = prog.newFreePoly(Lmonom);
+	% [prog,L23] = prog.newFreePoly(Lmonom);
+	% [prog,L24] = prog.newFreePoly(Lmonom);
+	% prog = prog.withSOS(L22);
+	% prog = prog.withSOS(L23);
+	% prog = prog.withSOS(L24);
+	% sum_rho=last_rho+delta_rho;
+	% r_sum_rho=1/sum_rho;
+	% r_delta_rho=1/delta_rho;
+	% r_last_rho=1/last_rho
+
+	% % the normals, without scaling (so that w is at the same scale as vertice values)
+	% w1=[r_last_rho*(last_vertice_values(1)-last_vertice_values(2));r_delta_rho*(this_vert_values(1)-last_vertice_values(1))];
+	% w2=[r_delta_rho*(last_vertice_values(2)-this_vert_values(2));r_sum_rho*(this_vert_values(1)-last_vertice_values(2))];
+	% w3=[r_delta_rho*(last_vertice_values(2)-this_vert_values(2));r_last_rho*(this_vert_values(2)-last_vertice_values(3))];
+	% w4=[]
+
+	% w1=[-Vertices_values(2);Vertices_values(1)];
+	% w2=[-Vertices_values(2);-Vertices_values(3)];
+	% w3=[Vertices_values(4);-Vertices_values(3)];
+	% w4=[Vertices_values(4);Vertices_values(1)];
+
+	% w1=[-Vertices_values(2);Vertices_values(1)];
+	% w2=[-Vertices_values(2);-Vertices_values(3)];
+	% w3=[Vertices_values(4);-Vertices_values(3)];
+	% w4=[Vertices_values(4);Vertices_values(1)];
+
+
+	% % slack variables, pushing the solution into the interior of the feasible set
+	% [prog,slack]=prog.newPos(4);
+
+	% % ring 1
+	% V1dot=w1'*xdot;
+	% constraint1=x(1);
+	% constraint2=-x(2);
+	% constraint3=-x(1)+x(2)-rho;
+	% prog=prog.withSOS(-slack(1)-V1dot+L1*constraint1+L2*constraint2+L3*constraint3);
+
+	% % ring 2
+	% V2dot=w2'*xdot;
+	% constraint4=x(1);
+	% constraint5=x(2);
+	% constraint6=-x(1)-x(2)-rho;
+	% prog=prog.withSOS(-slack(2)-V2dot+L4*constraint4+L5*constraint5+L6*constraint6);
+
+	% % ring 3
+	% V3dot=w3'*xdot;
+	% constraint7=x(2);
+	% constraint8=-x(1);
+	% constraint9=x(1)-x(2)-rho;
+	% prog=prog.withSOS(-slack(3)-V3dot+L7*constraint7+L8*constraint8+L9*constraint9);
+
+	% % ring 4
+	% V4dot=w4'*xdot;
+	% constraint10=-x(1);
+	% constraint11=-x(2);
+	% constraint12=x(1)+x(2)-rho;
+	% prog=prog.withSOS(-slack(4)-V4dot+L10*constraint10+L11*constraint11+L12*constraint12);
+	options = spot_sdp_default_options();
+	options.verbose=1;
+
+	% vert0=[0;0];
+	% vert1=[0;rho];
+	% vert2=[-rho;0];
+	% vert3=[0;-rho];
+	% vert4=[rho;0];
+	% v1dot_at_verticies=[subs(V1dot,x,vert1);subs(V1dot,x,vert2);subs(V2dot,x,vert2);subs(V2dot,x,vert3);subs(V3dot,x,vert3);subs(V3dot,x,vert4);subs(V4dot,x,vert4);subs(V4dot,x,vert1)];
+
+	sol=prog.minimize(sum(t),@spot_mosek,options);
+	if sol.status==spotsolstatus.STATUS_PRIMAL_AND_DUAL_FEASIBLE
+		sol_OK=true;
+		% Vertices_values=double(sol.eval(Vertices_values));
+		V1=((sol.eval(V1)))
+		V2=(sol.eval(V2))
+		V3=(sol.eval(V3))
+		V4=(sol.eval(V4))
+		% rho=double(sol.eval(rho));
+		% w=[w1,w2,w3,w4]./rho;
+		% rho_diff=rho-varargin{1};
+
+	else
+		sol_OK=false;
+		% rho=varargin{1};
+		% Vertices_values=varargin{2};
+		% w=varargin{3};
+		% rho_diff=0;
+	end
+end
+
+
 
 function [rho,Vertices_values,w,sol_OK]=diamond(x,xdot,df,method,varargin)
 	% disp(method);
@@ -160,7 +347,10 @@ function [rho,Vertices_values,w,sol_OK]=diamond(x,xdot,df,method,varargin)
 		rho=double(sol.eval(rho));
 		w=[w1,w2,w3,w4]./rho;
 		rho_diff=rho-varargin{1};
-	else
+	% else if sol.status == spotsolstatus.STATUS_SOLVER_ERROR
+	%   	error('The solver threw an internal error.');
+
+	else 
 		sol_OK=false;
 		rho=varargin{1};
 		Vertices_values=varargin{2};
@@ -171,6 +361,9 @@ function [rho,Vertices_values,w,sol_OK]=diamond(x,xdot,df,method,varargin)
 	% L=[(L1),(L2),(L3),(L4),(L5),(L6),(L7),(L8),(L9),(L10),(L11),(L12)];
 	% L=[sol.eval(L1),sol.eval(L2),sol.eval(L3),sol.eval(L4),sol.eval(L5),sol.eval(L6),sol.eval(L7),sol.eval(L8),sol.eval(L9),sol.eval(L10),sol.eval(L11),sol.eval(L12)];
 end
+
+
+
 
 function plots(w,xdot)
 	x1=-1:.01:0;
