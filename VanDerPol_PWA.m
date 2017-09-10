0c569432fd9df8edfe7a447af3b3722626056e61
Shen Shen <shenshen@mit.edu>
2017-09-09 22:43:06 -0400

    works v1

--- VanDerPol_PWA.m
+++ VanDerPol_PWA.m
@@ -8,18 +8,19 @@ function VanDerPol_PWA()
 	% xdot=[-x(1);-x(2)];
 	xdot = -[x(2); -x(1)-x(2).*(x(1).^2-1)];
 
-	rho=6e-3;
+	rho=3e-4;
+	delta_rho=3e-4;
 	Vertices_values=zeros(4,1);
 	w=zeros(2,4);
 	sol_OK=true;
-
-	% while(sol_OK)
-	% 	disp(iter_count);
-		% [rho,Vertices_values,w,sol_OK]=
-		diamond(x,xdot,do_plots,rho,Vertices_values,w)
-	% 	rho=1.2*rho;
-	% 	iter_count=iter_count+1;
- %    end
+	level=0;
+	[V,rho,all_V]=dis_diamond_ring(x,xdot,do_plots,0,level,rho);
+	level=1;
+	while(sol_OK)
+		disp(level);
+		[V,rho,all_V]=dis_diamond_ring(x,xdot,do_plots,rho,1,delta_rho,V,all_V);
+		level=level+1;
+    end
 
 	% diamond_ring(x,xdot,rho,[7.6180;7.5818;7.6180;7.5818],.2);
 % 	[rho,Vertices_values,w,sol_OK]=diamond_ring(x,xdot,rho,Vertices_values,1,do_plots);
@@ -27,12 +28,188 @@ function VanDerPol_PWA()
 
 end
 
-function diamond(x,xdot,do_plots,varargin)
+
+function [V,rho,all_V]=dis_diamond_ring(x,xdot,do_plots,varargin)
+	last_rho=varargin{1};
+	level=varargin{2};
+	delta_rho=varargin{3};
+	sum_rho=last_rho+delta_rho;
+	prog = spotsosprog;
+	prog = prog.withIndeterminate(x);
+
+	Vmonom = monomials(x,0:1);
+	[prog,V] = prog.newFreePoly(Vmonom,4);
+	
+	Lmonom = monomials(x,0:2);
+	[prog,L] = prog.newSOSPoly(Lmonom,16);
+
+	vert0=[0;0];
+	vert1=[0;last_rho];
+	vert2=[-last_rho;0];
+	vert3=[0;-last_rho];
+	vert4=[last_rho;0];
+	vert5=[0;last_rho+delta_rho];
+	vert6=[-last_rho-delta_rho;0];
+	vert7=[0;-last_rho-delta_rho];
+	vert8=[last_rho+delta_rho;0];
+	inner_verts=[vert1,vert2,vert3,vert4];
+	outter_verts=[vert5,vert6,vert7,vert8];
+
+	if level==0
+		% all V zero at zero
+		prog=prog.withEqs(subs(V,x,vert0)-zeros(4,1));
+		% all V strictly positive at verts
+		prog=prog.withPos((subs(V(1),x,vert5))-1e-6);
+		prog=prog.withPos((subs(V(4),x,vert5))-1e-6);
+		prog=prog.withPos((subs(V(1),x,vert6))-1e-6);
+		prog=prog.withPos((subs(V(2),x,vert6))-1e-6);
+		prog=prog.withPos((subs(V(2),x,vert7))-1e-6);
+		prog=prog.withPos((subs(V(3),x,vert7))-1e-6);
+		prog=prog.withPos((subs(V(3),x,vert8))-1e-6);
+		prog=prog.withPos((subs(V(4),x,vert8))-1e-6);
+	else
+		last_V=varargin{4};
+		all_V=varargin{5};
+		last_max=max((dmsubs(last_V,x,inner_verts)),[],2);
+		% evaluated at inner verts, last_V>=V
+		prog=prog.withPos(last_max(1)-(subs(V(1),x,vert1)));
+		prog=prog.withPos(last_max(1)-(subs(V(4),x,vert1)));
+		prog=prog.withPos(last_max(2)-(subs(V(1),x,vert2)));
+		prog=prog.withPos(last_max(2)-(subs(V(2),x,vert2)));
+		prog=prog.withPos(last_max(3)-(subs(V(2),x,vert3)));
+		prog=prog.withPos(last_max(3)-(subs(V(3),x,vert3)));
+		prog=prog.withPos(last_max(4)-(subs(V(3),x,vert4)));
+		prog=prog.withPos(last_max(4)-(subs(V(4),x,vert4)));
+		% V at outter verts are bigger than inner verts
+		prog=prog.withPos((subs(V(1),x,vert5-vert1)));
+		prog=prog.withPos((subs(V(4),x,vert5-vert1)));
+		prog=prog.withPos((subs(V(1),x,vert6-vert2)));
+		prog=prog.withPos((subs(V(2),x,vert6-vert2)));
+		prog=prog.withPos((subs(V(2),x,vert7-vert3)));
+		prog=prog.withPos((subs(V(3),x,vert7-vert3)));
+		prog=prog.withPos((subs(V(3),x,vert8-vert4)));
+		prog=prog.withPos((subs(V(4),x,vert8-vert4)));
+	end
+
+	w=diff(V,x);
+	% slack variables, pushing the solution into the interior of the feasible set
+	[prog,slack]=prog.newPos(4);
+
+	constraint1=[x(1);-x(2);-x(1)+x(2)-sum_rho;-(-x(1)+x(2)-last_rho)];
+	constraint2=[x(1);x(2);-x(1)-x(2)-sum_rho;-(-x(1)-x(2)-last_rho)];
+	constraint3=[x(2);-x(1);x(1)-x(2)-sum_rho;-(x(1)-x(2)-last_rho)];
+	constraint4=[-x(1);-x(2);x(1)+x(2)-sum_rho;-(x(1)+x(2)-last_rho)];
+
+	% polytope 1
+	V1dot=w(1,:)*xdot*delta_rho;
+	prog=prog.withSOS(-slack(1)-V1dot+L(1:4)'*constraint1);
+
+	% polytope 2
+	V2dot=w(2,:)*xdot*delta_rho;
+	prog=prog.withSOS(-slack(2)-V2dot+L(5:8)'*constraint2);
+
+	% polytope 3
+	V3dot=w(3,:)*xdot*delta_rho;
+	prog=prog.withSOS(-slack(3)-V3dot+L(9:12)'*constraint3);
+
+	% polytope 4
+	V4dot=w(4,:)*xdot*delta_rho;
+	prog=prog.withSOS(-slack(4)-V4dot+L(13:16)'*constraint4);
+	
+	options = spot_sdp_default_options();
+	options.verbose=1;
+	sol=prog.minimize(-sum(slack),@spot_mosek,options);
+
+	if sol.status==spotsolstatus.STATUS_PRIMAL_AND_DUAL_FEASIBLE
+		sol_OK=true;
+        V=sol.eval(V);
+        w=sol.eval(w);
+        rho=sum_rho;
+        figure(1)
+        % subplot(1,5,1)
+        clf
+        combined=[-x(1)+x(2)-sum_rho;-x(1)-x(2)-sum_rho;x(1)-x(2)-sum_rho;x(1)+x(2)-sum_rho;];
+        [a,b]=meshgrid(-sum_rho:sum_rho/100:sum_rho,-sum_rho:sum_rho/100:sum_rho);
+        regional=[min(dmsubs(combined,x,[a(:)';b(:)'])<=0)];
+        if level==0
+        	all_V=V;
+        	z=regional.*(dmsubs([V],x,[a(:)';b(:)']));
+        else
+        	all_V=[all_V;V];
+        	z=regional.*(dmsubs(all_V,x,[a(:)';b(:)']));
+        end
+        z=max(z,[],1);
+        z(z==0) = NaN;
+        z=reshape(z,size(a));
+        surf(a,b,z);
+        title('$$max(V)$$','interpreter','latex','fontsize',20) 
+        xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+        ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+        % subplot(1,5,2)
+        figure(2)
+        clf
+        contour(a,b,z);
+        title('$$ max(V)$$ contour','interpreter','latex','fontsize',20) 
+        xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+        ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+        % true piecewise
+        % regional=[min(dmsubs(constraint1,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint2,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint3,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint4,x,[a(:)';b(:)'])<=0)];
+        % z=(regional).*(dmsubs(V,x,[a(:)';b(:)']));
+        % z=max(z,[],1);
+        % z=reshape(z,size(a));
+        % z(z==0) = NaN;
+        % subplot(1,5,3)
+        % surf(a,b,z);
+        % title('piecewise affine $$V$$','interpreter','latex','fontsize',20) 
+        % xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+        % ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+        % subplot(1,5,4)
+        % contour(a,b,z);
+        % title('piecewise affine $$V$$ contour' ,'interpreter','latex','fontsize',20) 
+        % xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+        % ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+        % % Vdot plot
+        % subplot(1,5,5)
+        % V=sol.eval(V);
+        % w=sol.eval(w);
+        % regional=[min(dmsubs(constraint1,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint2,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint3,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint4,x,[a(:)';b(:)'])<=0)];
+        % z=(regional).*(dmsubs(w*xdot,x,[a(:)';b(:)']));
+        % z=sum(z,1);
+        % z=reshape(z,size(a));
+        % z(z==0) = NaN;
+        % surf(a,b,z);
+        % title('piecewise $$\dot{V}$$','interpreter','latex','fontsize',20) 
+        % xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+        % ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+	else 
+		sol_OK=false;
+		% falls back to the last valid rho
+		rho=varargin{1}/1.2;
+		Vertices_values=varargin{2};
+		w=varargin{3};
+		if do_plots
+			figure(2)
+			syms x1 x2;
+			V= piecewise(x1<=0&-x2<=0&-x1+x2-rho<=0,w(:,1)'*[x1;x2],x1<=0&x2<=0&-x1-x2-rho<=0,w(:,2)'*[x1;x2],x2<=0&-x1<=0&x1-x2-rho<=0,w(:,3)'*[x1;x2],-x1<=0&-x2<=0&x1+x2-rho<=0,w(:,4)'*[x1;x2],NaN);
+			fsurf(V); hold on
+			% fcontour(V);
+			figure(3)
+			% sym_xdot=[-2*x1+x1^3; -2*x2+x2^3];
+			sym_xdot =-[x2; -x1-x2.*(x1.^2-1)];
+			Vdot=piecewise(x1<=0&-x2<=0&-x1+x2-rho<=0,w(:,1)'*sym_xdot,x1<=0&x2<=0&-x1-x2-rho<=0,w(:,2)'*sym_xdot,x2<=0&-x1<=0&x1-x2-rho<=0,w(:,3)'*sym_xdot,-x1<=0&-x2<=0&x1+x2-rho<=0,w(:,4)'*sym_xdot,NaN);
+			fsurf(Vdot); hold on
+		end
+	end
+end
+
+
+
+function [V,rho]=discontinuous_diamond(x,xdot,do_plots,varargin)
 	prog = spotsosprog;
 	prog = prog.withIndeterminate(x);
 	rho=varargin{1};
-	% [prog,Vertices_values] = prog.newPos(8);
-	% prog=prog.withPos(Vertices_values()-1e-6*ones(8,1));
+	level=varargin{2};
+	last_V=varargin{3};
 
 	Vmonom = monomials(x,0:1);
 	[prog,V] = prog.newFreePoly(Vmonom,4);
@@ -41,7 +218,6 @@ function diamond(x,xdot,do_plots,varargin)
 	[prog,L] = prog.newSOSPoly(Lmonom,12);
 
 	vert0=[0;0];
-
 	vert1=[0;rho];
 	vert2=[-rho;0];
 	vert3=[-rho;0];
@@ -50,7 +226,9 @@ function diamond(x,xdot,do_plots,varargin)
 	vert6=[rho;0];
 	vert7=[rho;0];
 	vert8=[0;rho];
-% all V zero at zero
+
+
+
 	prog=prog.withEqs(subs(V,x,vert0)-zeros(4,1));
 % all V strictly positive at verts
 	prog=prog.withPos((subs(V(1),x,vert1))-1e-6);
@@ -97,11 +275,12 @@ function diamond(x,xdot,do_plots,varargin)
 	options = spot_sdp_default_options();
 	options.verbose=1;
 	sol=prog.minimize(-sum(slack),@spot_mosek,options);
-
 	if sol.status==spotsolstatus.STATUS_PRIMAL_AND_DUAL_FEASIBLE
 		sol_OK=true;
         V=sol.eval(V);
         w=sol.eval(w);
+        figure(1)
+        subplot(1,5,1)
         combined=[-x(1)+x(2)-rho;-x(1)-x(2)-rho;x(1)-x(2)-rho;x(1)+x(2)-rho;];
         [a,b]=meshgrid(-rho:rho/100:rho,-rho:rho/100:rho);
         regional=[min(dmsubs(combined,x,[a(:)';b(:)'])<=0)];
@@ -109,19 +288,18 @@ function diamond(x,xdot,do_plots,varargin)
         z=max(z,[],1);
         z(z==0) = NaN;
         z=reshape(z,size(a));
-        figure(1)
-        subplot(1,5,1)
         surf(a,b,z);
         title('$$max(V)$$','interpreter','latex','fontsize',20) 
         xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
         ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
-
+        hold on;
         subplot(1,5,2)
         contour(a,b,z);
         title('$$ max(V)$$ contour','interpreter','latex','fontsize',20) 
         xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
         ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
-
+        hold on;
+        % true piecewise
         regional=[min(dmsubs(constraint1,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint2,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint3,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint4,x,[a(:)';b(:)'])<=0)];
         z=(regional).*(dmsubs(V,x,[a(:)';b(:)']));
         z=max(z,[],1);
@@ -132,14 +310,12 @@ function diamond(x,xdot,do_plots,varargin)
         title('piecewise affine $$V$$','interpreter','latex','fontsize',20) 
         xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
         ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
-
-
         subplot(1,5,4)
         contour(a,b,z);
         title('piecewise affine $$V$$ contour' ,'interpreter','latex','fontsize',20) 
         xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
         ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
-
+        % Vdot plot
         subplot(1,5,5)
         V=sol.eval(V);
         w=sol.eval(w);
@@ -175,7 +351,7 @@ end
 
 
 
-function [rho,Vertices_values,w,sol_OK]=diamond_ring(x,xdot,df,last_rho,last_vertice_values,delta_rho,do_plots)
+function [rho,Vertices_values,w,sol_OK]=continuous_diamond_ring(x,xdot,df,last_rho,last_vertice_values,delta_rho,do_plots)
 	prog = spotsosprog;
 	prog = prog.withIndeterminate(x);
 	[prog,this_vert_values] = prog.newPos(4);
