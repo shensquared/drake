19ad70f3129e78248b1ccb2c2cece70c28a8d3e1
Shen Shen <shenshen@mit.edu>
2017-09-11 22:41:29 -0400

    remove MAX plot; add square plots

--- plots_stuff.m
+++ plots_stuff.m
@@ -1,48 +1,42 @@
-function plots_stuff(x,xdot,V,all_V,last_rho,delta_rho)
+function plots_stuff(x,xdot,V,all_V,last_rho,delta_rho,flags)
         sum_rho=last_rho+delta_rho;
-        constraint1=[x(1);-x(2);-x(1)+x(2)-sum_rho;-(-x(1)+x(2)-last_rho)];
-        constraint2=[x(1);x(2);-x(1)-x(2)-sum_rho;-(-x(1)-x(2)-last_rho)];
-        constraint3=[x(2);-x(1);x(1)-x(2)-sum_rho;-(x(1)-x(2)-last_rho)];
-        constraint4=[-x(1);-x(2);x(1)+x(2)-sum_rho;-(x(1)+x(2)-last_rho)];
-        figure(1)
-        subplot(1,4,1)
-        combined=[-x(1)+x(2)-sum_rho;-x(1)-x(2)-sum_rho;x(1)-x(2)-sum_rho;x(1)+x(2)-sum_rho;];
+
+        switch flags.method
+        case 'flat'        
+                constraint1=[x(1);-x(2);-x(1)+x(2)-sum_rho;-(-x(1)+x(2)-last_rho)];
+                constraint2=[x(1);x(2);-x(1)-x(2)-sum_rho;-(-x(1)-x(2)-last_rho)];
+                constraint3=[x(2);-x(1);x(1)-x(2)-sum_rho;-(x(1)-x(2)-last_rho)];
+                constraint4=[-x(1);-x(2);x(1)+x(2)-sum_rho;-(x(1)+x(2)-last_rho)];
+                combined=[-x(1)+x(2)-sum_rho;-x(1)-x(2)-sum_rho;x(1)-x(2)-sum_rho;x(1)+x(2)-sum_rho;];
+        case 'square'
+                constraint1=[-sum_rho-x(1);x(1)+x(2);x(1)-x(2);-(-last_rho-x(1))];
+                constraint2=[-sum_rho-x(2);x(2)-x(1);x(1)+x(2);-(-last_rho-x(2))];
+                constraint3=[x(2)-x(1);-x(1)-x(2);x(1)-sum_rho;-(x(1)-last_rho)];
+                constraint4=[x(2)-sum_rho;-x(1)-x(2);x(1)-x(2);-(x(2)-last_rho)];
+                combined=[-sum_rho-x(1);-sum_rho-x(2);x(1)-sum_rho;x(2)-sum_rho;]
+        end
         [a,b]=meshgrid(-sum_rho:sum_rho/100:sum_rho,-sum_rho:sum_rho/100:sum_rho);
-        regional=[min(dmsubs(combined,x,[a(:)';b(:)'])<=0)];
-        z=(dmsubs(V,x,[a(:)';b(:)']));
-        z=regional.*max(z,[],1);
-        z(z==0) = NaN;
-        z=reshape(z,size(a));
-        surf(a,b,z);
-        title('$$max(V)$$','interpreter','latex','fontsize',20) 
-        xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
-        ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
-        hold on;
-        subplot(1,4,2)
-        contour(a,b,z);
-        title('$$ max(V)$$ contour','interpreter','latex','fontsize',20) 
-        xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
-        ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
-        hold on;
+
+        figure(1)
         % true piecewise
         regional=[min(dmsubs(constraint1,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint2,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint3,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint4,x,[a(:)';b(:)'])<=0)];
         z=(regional).*(dmsubs(V,x,[a(:)';b(:)']));
         z=max(z,[],1);
         z=reshape(z,size(a));
         z(z==0) = NaN;
-        subplot(1,4,3)
+        subplot(1,3,1)
         surf(a,b,z);
         title('piecewise affine $$V$$','interpreter','latex','fontsize',20) 
         xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
         ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
-        subplot(1,4,4)
+        subplot(1,3,2)
         contour(a,b,z);
         title('piecewise affine $$V$$ contour' ,'interpreter','latex','fontsize',20) 
         xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
         ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
-        figure(2)
+        % figure(2)
         % Vdot plot
-        % subplot(1,5,5)
+        subplot(1,3,3)
         w=diff(V,x);
         regional=[min(dmsubs(constraint1,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint2,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint3,x,[a(:)';b(:)'])<=0);min(dmsubs(constraint4,x,[a(:)';b(:)'])<=0)];
         z=(regional).*(dmsubs(w*xdot,x,[a(:)';b(:)']));
