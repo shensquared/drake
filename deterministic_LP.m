6e6013da8a858caf47cca37320d7cf2a564db95a
Shen Shen <shenshen@mit.edu>
2017-09-15 10:09:39 -0400

    wip goof around

--- deterministic_LP.m
+++ deterministic_LP.m
@@ -1,6 +1,14 @@
 function deterministic_LP()
-	rho=.9;
-	resolution=18;
+	rho=.1;
+	resolution=20;
+
+	checkDependency('spotless');
+	checkDependency('mosek');
+	x=msspoly('x',2);
+	% xdot = [-2*x(1); -2*x(2)];
+	% xdot = [-2*x(1)+x(1)^2; -x(2)+x(2)^3];
+	xdot = -[x(2); -x(1)-x(2).*(x(1).^2-1)];
+
 	row_verts=2*resolution+1;
 	num_tris=(2*resolution)^2;
 	num_verts=(row_verts)^2;
@@ -14,14 +22,12 @@ function deterministic_LP()
 	centroid_c=reshape(centroid_c,[num_tris,1]);
 	centroid_d=reshape(centroid_d,[num_tris,1]);
 	% clf
+	
 	% scatter(reshape(a,[num_verts,1]),reshape(b,[num_verts,1])); hold on
 	% scatter(centroid_a,centroid_b); hold on
 	% scatter(centroid_c,centroid_d); hold on
 
-	x=msspoly('x',2);
-	% xdot = [-2*x(1); -2*x(2)];
-	% xdot = [-2*x(1)+x(1)^3; -2*x(2)+x(2)^3];
-	xdot = -[x(2); -x(1)-x(2).*(x(1).^2-1)];
+
 
 	df=diff(xdot,x);
 	A=[];
@@ -30,16 +36,19 @@ function deterministic_LP()
 	for i =1:num_tris
 		A=[A,A1(:,i),A2(:,i)];
 	end
+
 	B=[];
 	B1=dmsubs(df(:,1),x,[centroid_c(:)';centroid_d(:)']);
 	B2=dmsubs(df(:,2),x,[centroid_c(:)';centroid_d(:)']);
 	for i =1:num_tris
 		B=[B,B1(:,i),B2(:,i)];
 	end
-	vert_values=find_V(x,xdot,A,B,rho,resolution,a,b);
+
+	quiver(centroid_a,centroid_b,)
+	% vert_values=find_V(x,xdot,A,B,rho,resolution,a,b,centroid_a,centroid_b,centroid_c,centroid_d);
 end
 
-function vert_values=find_V(x,xdot,A,B,rho,resolution,a,b)
+function vert_values=find_V(x,xdot,A,B,rho,resolution,a,b,centroid_a,centroid_b,centroid_c,centroid_d)
 	row_verts=2*resolution+1;
 	num_tris=(2*resolution)^2;
 	num_verts=(row_verts)^2;
@@ -69,40 +78,49 @@ function vert_values=find_V(x,xdot,A,B,rho,resolution,a,b)
 		column_diff=[column_diff,vert_values(:,i+1)-vert_values(:,i)];
 		row_diff=[row_diff;vert_values(i+1,:)-vert_values(i,:)];
 	end
-	xrep=repmat(x,[num_tris,1]);
 
 % % lower triangles
-	w1=column_diff(2:row_verts,:);
+	w1=column_diff(1:row_verts-1,:);
 	w2=row_diff(:,1:row_verts-1);
 	w1=reshape(w1,num_tris,1);
 	w2=reshape(w2,num_tris,1);
-	Vdot=[w1,w2]*A*xrep;
+    w=[w1,w2];
+	Vdot=w(1,:)*A(:,1:2)*x;
+	for i=2:num_tris
+		Vdot=[Vdot;w(i,:)*A(:,2*i-1:2*i)*x];
+	end
+            
+    
 	Vdot=reshape(Vdot,[2*resolution,2*resolution]);
 % upper triangless
-	n1=column_diff(1:row_verts-1,:);
+	n1=column_diff(2:row_verts,:);
 	n2=row_diff(:,2:row_verts);
 	n1=reshape(n1,num_tris,1);
 	n2=reshape(n2,num_tris,1);
-	Vdot_B=[n1,n2]*B*xrep;
+    n=[n1,n2];
+	Vdot_B=n(1,:)*B(:,1:2)*x;
+	for i=2:num_tris
+		Vdot_B=[Vdot_B;n(i,:)*B(:,2*i-1:2*i)*x];
+    end
+	% Vdot_B=[n1,n2]*B*xrep;
 	Vdot_B=reshape(Vdot_B,[2*resolution,2*resolution]);
 
+	a=reshape(a,[num_verts,1]);
+	b=reshape(b,[num_verts,1]);
+	grids=[a';b'];
+	grids=reshape(grids,[2,row_verts,row_verts]);
 	for i=1:2*resolution
 		for j=1:2*resolution
-			prog=prog.withPos(-(subs(Vdot(i,j),x,[a(i,j);b(i,j)])));
-			prog=prog.withPos(-(subs(Vdot(i,j),x,[a(i+1,j);b(i+1,j)])));
-			prog=prog.withPos(-(subs(Vdot(i,j),x,[a(i+1,j+1);b(i+1,j+1)])));
+			% lower tri
+			prog=prog.withPos(-(subs(Vdot(i,j),x,grids(:,i+1,j))));
+			prog=prog.withPos(-(subs(Vdot(i,j),x,grids(:,i,j+1))));
+			prog=prog.withPos(-(subs(Vdot(i,j),x,grids(:,i+1,j+1))));
+			% upper tri
+			prog=prog.withPos(-(subs(Vdot_B(i,j),x,grids(:,i,j+1))));
+			prog=prog.withPos(-(subs(Vdot_B(i,j),x,grids(:,i+1,j+1))));
+			prog=prog.withPos(-(subs(Vdot_B(i,j),x,grids(:,i+1,j))));
 		end
 	end
-
-	% for i=1:2*resolution
-	% 	for j=1:2*resolution
-	% 		% verts=[(a(i,j);b(i,j));;(a(i+1,j);b(i+1,j))];
-	% 		prog=prog.withPos(-(subs(Vdot_B(i,j),x,[a(i,j);b(i,j)])));
-	% 		prog=prog.withPos(-(subs(Vdot_B(i,j),x,[a(i,j+1);b(i,j+1)])));
-	% 		prog=prog.withPos(-(subs(Vdot_B(i,j),x,[a(i+1,j+1);b(i+1,j+1)])));
-
-	% 	end
-	% end
 
 	% slacks=reshape(slacks,[num_tris,1]);
 	% slacks_B=reshape(slacks_B,[num_tris,1]);
@@ -116,7 +134,7 @@ function vert_values=find_V(x,xdot,A,B,rho,resolution,a,b)
 		values=double(sol.eval(vert_values))
 		tri = delaunay(a,b);
 		trisurf(tri,a,b,values);
-		title('piecewise $$\dot{V}$$','interpreter','latex','fontsize',20) 
+		title('piecewise affine $${V}$$','interpreter','latex','fontsize',20) 
 		xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
 		ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
 	end
