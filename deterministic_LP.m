2d4e4d1f3aadffd73695f1658daf899e2ddb25bd
Shen Shen <shenshen@mit.edu>
2017-09-15 15:04:44 -0400

    WIP fixes linearization

--- deterministic_LP.m
+++ deterministic_LP.m
@@ -1,13 +1,13 @@
 function deterministic_LP()
+	findV=true;
+	pause_sec=0;
+	
 	rho=.1;
-	resolution=20;
+	resolution=5;
 
 	checkDependency('spotless');
 	checkDependency('mosek');
 	x=msspoly('x',2);
-	% xdot = [-2*x(1); -2*x(2)];
-	% xdot = [-2*x(1)+x(1)^2; -x(2)+x(2)^3];
-	xdot = -[x(2); -x(1)-x(2).*(x(1).^2-1)];
 
 	row_verts=2*resolution+1;
 	num_tris=(2*resolution)^2;
@@ -16,39 +16,73 @@ function deterministic_LP()
 	[a,b]=meshgrid(-rho:rho/resolution:rho,-rho:rho/resolution:rho);
 	[centroid_a,centroid_b]=meshgrid((-3+1/resolution)*rho/3:rho/resolution:(3-2/resolution)*rho/3,(-3+1/resolution)*rho/3:rho/resolution:(3-2/resolution)*rho/3);
 	[centroid_c,centroid_d]=meshgrid((-3+2/resolution)*rho/3:rho/resolution:(3-1/resolution)*rho/3,(-3+2/resolution)*rho/3:rho/resolution:(3-1/resolution)*rho/3);
-
 	centroid_a=reshape(centroid_a,[num_tris,1]);
 	centroid_b=reshape(centroid_b,[num_tris,1]);
 	centroid_c=reshape(centroid_c,[num_tris,1]);
 	centroid_d=reshape(centroid_d,[num_tris,1]);
-	% clf
 	
-	% scatter(reshape(a,[num_verts,1]),reshape(b,[num_verts,1])); hold on
+
+	% xdot = [-2*x(1); -2*x(2)];
+	xdot = [-x(1)+x(1)^3; -x(2)+x(2)^3];
+	% xdot = -[x(2); -x(1)-x(2).*(x(1).^2-1)];
+	A=[];Ax=[];bias_A=[];B=[];Bx=[];bias_B=[];
+	for i =1:num_tris
+		shifted_xdot=subs(xdot,x,[x(1)+centroid_a(i);x(2)+centroid_b(i)]);
+		current_df=diff(shifted_xdot,x);
+		current_centroid=[centroid_a(i)';centroid_b(i)'];
+		current_bias=subs(xdot,x,current_centroid);
+		bias_A=[bias_A,current_bias];
+		A1=dmsubs(current_df(:,1),x,zeros(2,1));
+		A2=dmsubs(current_df(:,2),x,zeros(2,1));
+		current_A=[A1,A2];
+		A=[A,A1,A2];
+		% Ax=[Ax,subs(current_A,x,zeros(2,1))];
+
+		shifted_xdot=subs(xdot,x,[x(1)-centroid_c(i);x(2)-centroid_d(i)]);
+		current_df=diff(shifted_xdot,x);
+		current_centroid=[centroid_c(i)';centroid_d(i)'];
+		current_bias=subs(xdot,x,current_centroid);
+		bias_B=[bias_B,current_bias];
+		B1=dmsubs(current_df(:,1),x,zeros(2,1));
+		B2=dmsubs(current_df(:,2),x,zeros(2,1));
+		current_B=[B1,B2];
+		B=[B,B1,B2];
+		% Bx=[Bx,subs(current_B,x,zeros(2,1))];
+	end
+
+	% figure('units','normalized','outerposition',[0 0 1 1])
+	% clf
+	% scatter(reshape(a,[num_verts,1]),reshape(b,[num_verts,1]));
+	% 	xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+	% 	ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+	% axis equal tight
+	% hold on
+	% pause(pause_sec);
 	% scatter(centroid_a,centroid_b); hold on
+	% 	xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+	% 	ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+	% pause(pause_sec);
+	% quiver(centroid_a,centroid_b,Ax(1,:)',Ax(2,:)'); hold on
+	% 	xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+	% 	ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+	% pause(pause_sec);
 	% scatter(centroid_c,centroid_d); hold on
+	% 	xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+	% 	ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+	% pause(pause_sec);
+	% quiver(centroid_c,centroid_d,Bx(1,:)',Bx(2,:)'); hold on
+	% 	xlabel('$$ x_1 $$','interpreter','latex','fontsize',15)
+	% 	ylabel('$$ x_2 $$','interpreter','latex','fontsize',15)
+	% % quiver(a,b,-a+a.^3,-b+b.^3);
+	% quiver(a,b,-b,-(-a-b.*(a.^2-1)));
 
-
-
-	df=diff(xdot,x);
-	A=[];
-	A1=dmsubs(df(:,1),x,[centroid_a(:)';centroid_b(:)']);
-	A2=dmsubs(df(:,2),x,[centroid_a(:)';centroid_b(:)']);
-	for i =1:num_tris
-		A=[A,A1(:,i),A2(:,i)];
+	if findV
+		vert_values=find_V(x,xdot,A,B,rho,resolution,a,b,centroid_a,centroid_b,centroid_c,centroid_d,bias_A,bias_B);
 	end
-
-	B=[];
-	B1=dmsubs(df(:,1),x,[centroid_c(:)';centroid_d(:)']);
-	B2=dmsubs(df(:,2),x,[centroid_c(:)';centroid_d(:)']);
-	for i =1:num_tris
-		B=[B,B1(:,i),B2(:,i)];
-	end
-
-	quiver(centroid_a,centroid_b,)
-	% vert_values=find_V(x,xdot,A,B,rho,resolution,a,b,centroid_a,centroid_b,centroid_c,centroid_d);
 end
 
-function vert_values=find_V(x,xdot,A,B,rho,resolution,a,b,centroid_a,centroid_b,centroid_c,centroid_d)
+
+function vert_values=find_V(x,xdot,A,B,rho,resolution,a,b,centroid_a,centroid_b,centroid_c,centroid_d,bias_A,bias_B)
 	row_verts=2*resolution+1;
 	num_tris=(2*resolution)^2;
 	num_verts=(row_verts)^2;
@@ -85,46 +119,52 @@ function vert_values=find_V(x,xdot,A,B,rho,resolution,a,b,centroid_a,centroid_b,
 	w1=reshape(w1,num_tris,1);
 	w2=reshape(w2,num_tris,1);
     w=[w1,w2];
-	Vdot=w(1,:)*A(:,1:2)*x;
+	Vdot=w(1,:)*A(:,1:2)*x+w(1,:)*bias_A(:,1);
 	for i=2:num_tris
-		Vdot=[Vdot;w(i,:)*A(:,2*i-1:2*i)*x];
+		Vdot=[Vdot;w(i,:)*A(:,2*i-1:2*i)*x+w(i,:)*bias_A(:,i)];
 	end
-            
-    
 	Vdot=reshape(Vdot,[2*resolution,2*resolution]);
+
 % upper triangless
 	n1=column_diff(2:row_verts,:);
 	n2=row_diff(:,2:row_verts);
 	n1=reshape(n1,num_tris,1);
 	n2=reshape(n2,num_tris,1);
     n=[n1,n2];
-	Vdot_B=n(1,:)*B(:,1:2)*x;
+	Vdot_B=n(1,:)*B(:,1:2)*x+n(1,:)*bias_B(:,1);
 	for i=2:num_tris
-		Vdot_B=[Vdot_B;n(i,:)*B(:,2*i-1:2*i)*x];
+		Vdot_B=[Vdot_B;n(i,:)*B(:,2*i-1:2*i)*x+n(i,:)*bias_B(:,i)];
     end
-	% Vdot_B=[n1,n2]*B*xrep;
 	Vdot_B=reshape(Vdot_B,[2*resolution,2*resolution]);
 
 	a=reshape(a,[num_verts,1]);
 	b=reshape(b,[num_verts,1]);
 	grids=[a';b'];
 	grids=reshape(grids,[2,row_verts,row_verts]);
+
+	centroid_a=reshape(centroid_a,[2*resolution,2*resolution]);
+	centroid_b=reshape(centroid_b,[2*resolution,2*resolution]);
+	centroid_c=reshape(centroid_c,[2*resolution,2*resolution]);
+	centroid_d=reshape(centroid_d,[2*resolution,2*resolution]);
+
+% Vdot<0 at grids
 	for i=1:2*resolution
 		for j=1:2*resolution
 			% lower tri
-			prog=prog.withPos(-(subs(Vdot(i,j),x,grids(:,i+1,j))));
-			prog=prog.withPos(-(subs(Vdot(i,j),x,grids(:,i,j+1))));
-			prog=prog.withPos(-(subs(Vdot(i,j),x,grids(:,i+1,j+1))));
+			prog=prog.withPos(-slacks(i,j)-(subs(Vdot(i,j),x,grids(:,i+1,j)-[centroid_a(i,j);centroid_b(i,j)])));
+			prog=prog.withPos(-slacks(i,j)-(subs(Vdot(i,j),x,grids(:,i,j+1)-[centroid_a(i,j);centroid_b(i,j)])));
+			prog=prog.withPos(-slacks(i,j)-(subs(Vdot(i,j),x,grids(:,i+1,j+1)-[centroid_a(i,j);centroid_b(i,j)])));
 			% upper tri
-			prog=prog.withPos(-(subs(Vdot_B(i,j),x,grids(:,i,j+1))));
-			prog=prog.withPos(-(subs(Vdot_B(i,j),x,grids(:,i+1,j+1))));
-			prog=prog.withPos(-(subs(Vdot_B(i,j),x,grids(:,i+1,j))));
+			prog=prog.withPos(-slacks_B(i,j)-(subs(Vdot_B(i,j),x,grids(:,i,j+1)-[centroid_c(i,j);centroid_d(i,j)])));
+			prog=prog.withPos(-slacks_B(i,j)-(subs(Vdot_B(i,j),x,grids(:,i+1,j+1)-[centroid_c(i,j);centroid_d(i,j)])));
+			prog=prog.withPos(-slacks_B(i,j)-(subs(Vdot_B(i,j),x,grids(:,i+1,j)-[centroid_c(i,j);centroid_d(i,j)])));
 		end
 	end
 
-	% slacks=reshape(slacks,[num_tris,1]);
-	% slacks_B=reshape(slacks_B,[num_tris,1]);
 
+% solving stage
+	slacks=reshape(slacks,[num_tris,1]);
+	slacks_B=reshape(slacks_B,[num_tris,1]);
 	options = spot_sdp_default_options();
 	options.verbose=1;
 	sol=prog.minimize(0,@spot_mosek,options);
